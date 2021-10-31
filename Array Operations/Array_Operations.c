5bf75c35e5dd98a6969a3403726c4401ed55ee0d
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-01 05:25:57 +0530

    Beautifying code

--- Array Operations/Array_Operations.c
+++ Array Operations/Array_Operations.c
@@ -3,19 +3,19 @@
 #include<stdio.h>
 void main() {
     int a[100], i,j,n,pos,element;
-    printf("Enter the size of the array");
+    printf("Enter the size of the array\n");
     scanf("%d",&n);
-    printf("Enter the elements of the array");
+    printf("Enter the elements of the array\n");
     for(i=0;i<n;i++)
         scanf("%d",&a[i]);
 
 /* Traversal */
-    printf("The elements of the array are");
+    printf("The elements of the array are\n");
     for(i=0;i<n;i++)
         printf("%d\n",a[i]);
 
 /* Insertion */
-    printf("\nEnter the position and element to be inserted");  
+    printf("\nEnter the position and element to be inserted\n");  
     scanf("%d%d",&pos,&element);
     if(pos>n)
     printf("Insertion not possible");
@@ -23,13 +23,13 @@ void main() {
     for(i=n;i>=pos;i--)
         a[i+1]=a[i];
     a[pos]=element;
-    printf("\nThe elements of the array after insertion are");
+    printf("\nThe elements of the array after insertion are\n");
     for(i=0;i<=n;i++)
         printf("%d\n",a[i]);
     }
 
 /* Deletion */
-    printf("\nEnter the position to be deleted");
+    printf("\nEnter the position to be deleted\n");
     scanf("%d",&pos);
     if(pos>n)
     printf("Deletion not possible");
@@ -37,13 +37,13 @@ void main() {
         a[pos]=a[pos+1];
     for(i=pos+1;i<=n;i++)
         a[i]=a[i+1];
-    printf("\nThe elements of the array after deletion are");
+    printf("\nThe elements of the array after deletion are\n");
     for(i=0;i<n;i++)
         printf("%d\n",a[i]);
     }
 
 /*Search */
-    printf("\nEnter the element to be searched");
+    printf("\nEnter the element to be searched\n");
     scanf("%d",&element);
     for(i=0;i<n;i++) {
         if(a[i]==element)
@@ -52,13 +52,13 @@ void main() {
             printf("\nElement not found");
     }
 /* Update */
-    printf("\nEnter the position and element to be updated");
+    printf("\nEnter the position and element to be updated\n");
     scanf("%d%d",&pos,&element);
     if(pos>n)
     printf("Update not possible");
     else {
         a[pos]=element;
-        printf("\nThe elements of the array after update are");
+        printf("\nThe elements of the array after update are\n");
         for(i=0;i<n;i++)
             printf("%d\n",a[i]);
     }

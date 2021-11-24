0e65aaf92520e9cbd43f082503ea765b3c2b3ec0
Shibin Thomas <52344717+itsmeshibintmz@users.noreply.github.com>
2021-11-24 19:52:58 +0530

    Comments

--- Merge Two Sorted Arrays/Merge_Two_Sorted_Arrays.c
+++ Merge Two Sorted Arrays/Merge_Two_Sorted_Arrays.c
@@ -1,21 +1,25 @@
-/* C Program to sort and merge two arrays */
+/* C Program to merge two sorted arrays and store in
+    a third array. */
 
 #include <stdio.h>
 void main() {
     int i, j, k,m,n,temp;
+    /* Input the size of the first array */
     printf("Enter the number of elements in the first array\n");
     scanf("%d",&m);
     int a[m];
+    /* Input the elements of the first array */
     printf("Enter the elements of the first array\n");
     for(i=0;i<m;i++) {
         scanf("%d",&a[i]);
     }
+    /* Display the elements of the first array */
     printf("\nOriginal First Array is\n");
     for(i=0;i<m;i++) { 
         printf("%d\t",a[i]);
     }
 
-/* Sort the first array */
+    /* Sort the first array */
     for(i=0;i<m;i++) {
         for(j=i+1;j<m;j++) {
             if(a[i]>a[j]) {
@@ -25,24 +29,27 @@ void main() {
             }
         }
     }
+    /* Display the elements of the sorted first array */
     printf("\nSorted First Array is\n");
     for(i=0;i<m;i++) {
         printf("%d\t",a[i]);
     }
-
+    /* Input the size of the second array */
     printf("\nEnter the number of elements in the second array\n");
     scanf("%d",&n);
     int b[n],c[m+n];
+    /* Input the elements of the second array */
     printf("\nEnter the elements of the second array\n");
     for(i=0;i<n;i++) { 
         scanf("%d",&b[i]);
     }
+    /* Display the elements of the second array */
     printf("\nOriginal Second Array is\n");  
     for(i=0;i<n;i++) { 
         printf("%d\t",b[i]);
     }
 
-/* Sort the second array */
+    /* Sort the second array */
     for(i=0;i<n;i++) {
         for(j=i+1;j<n;j++) {
             if(b[i]>b[j]) {
@@ -52,12 +59,13 @@ void main() {
             }
         }
     }
+    /* Display the elements of the sorted second array */
     printf("\nSorted second array is\n");
     for(i=0;i<n;i++) {
         printf("%d\t",b[i]);
     } 
 
-/* Merge and sort two arrays into third One */
+    /* Merge and sort two arrays into third One */
     i=j=k=0;
     for(i=0;i<m&&j<n;) {
         if(a[i]<b[j]) {
@@ -81,6 +89,7 @@ void main() {
         k++;
         j++;
     }
+    /* Display the elements of the merged and sorted array */
     printf("\nMerged and sorted array is\n");
     for(i=0;i<k;i++) {
         printf("%d\t",c[i]);
